eaffd7fb6f7af30c4ec71e1ccd25250f0dc1b88a
Sonu Kumar <sonu@enacteservices.com>
2022-02-15 00:30:56 +0530

    image-card-created

--- app/src/main/java/com/sonu/jetpackcomposeexamples/MainActivity.kt
+++ app/src/main/java/com/sonu/jetpackcomposeexamples/MainActivity.kt
@@ -3,65 +3,81 @@ package com.sonu.jetpackcomposeexamples
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sonu.jetpackcomposeexamples.ui.theme.JetpackComposeExamplesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
-                Column(
+            val painter = painterResource(id = R.drawable.kermit)
+            Box(modifier = Modifier.fillMaxWidth(0.5f).padding(10.dp)) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = "Kermit is playing",
+                    title = "Kermit is playing in the snow"
+                )
+            }
+        }
+    }
+
+    @Composable
+    fun ImageCard(
+        painter: Painter,
+        contentDescription: String,
+        title: String,
+        modifier: Modifier = Modifier
+    ) {
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            elevation = 10.dp
+        ) {
+            Box(modifier = Modifier.height(200.dp)) {
+                Image(painter = painter, contentDescription = contentDescription, contentScale = ContentScale.Crop)
+                Box(
                     modifier = Modifier
-                        .padding(10.dp)
-                        .border(10.dp, color = Color.Blue)
-                        .padding(10.dp)
-                        .fillMaxWidth()
-//                        .width(1000.dp) // set the maximum available with if require width is not available
-//                        .requiredWidth(1000.dp) // in actual it gives the width
-                        .fillMaxHeight(0.5f)
-                        .background(color = Color.Yellow),
-                    verticalArrangement = Arrangement.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally //with require width text is not visible bec they are out of screen
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent, Color.Black
+                                ),
+                                startY = 300f
+                            )
+                        )
                 ) {
-                    Text(
-                        text = "Hello",
-                        modifier = Modifier.offset(x = 0.dp, y = 20.dp)
-                    ) // offset is not same as margin there is no margin in compose
-                    //offset just moves the given item it does not care about other item can overlap other item
-                    Spacer(modifier = Modifier.height(20.dp))
-                    Text(text = "Jetpack")
-                    Text(text = "Compose")
+
                 }
-
-                Row(
-
+                Box(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight()
-                        .background(color = Color.Red),
-                    horizontalArrangement = Arrangement.SpaceAround,
-                    verticalAlignment = Alignment.CenterVertically
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    contentAlignment = Alignment.BottomStart
                 ) {
-                    Text(text = "Hello")
-                    Text(text = "Jetpack")
-                    Text(text = "Compose")
+                    Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
                 }
             }
-
         }
-
-
     }
 }
